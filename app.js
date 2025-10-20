0685bdad9610d2122b45c527cf67b7efd1710b0c
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2025-10-20 11:38:03 +0545

    Created blog api

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDatabase = require("./database");
+
 const app = express();
+app.use(express.json());
 
 connectToDatabase();
 
@@ -9,8 +11,11 @@ app.get("/", (req, res) => {
   res.json({ message: "This is Homepage" });
 });
 
-app.get("/about", (req, res) => {
-  res.json({ message: "About Page" });
+app.post("/blog", (req, res) => {
+  const {title,subtitle,description,image} = req.body;
+  res.status(200).json({
+    message: "Blog API hit successfully",
+  });
 });
 
 app.listen(process.env.PORT, () => {
