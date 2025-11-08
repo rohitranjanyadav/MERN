da94f890e0132599670de1bdbe5f34afaa5e0bfe
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2025-11-08 14:13:16 +0545

    update blog

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 const { multer, storage } = require("./middleware/multerConfig");
 const upload = multer({ storage: storage });
+const fs = require("fs");
 const cors = require("cors");
 
 app.use(
@@ -67,10 +68,45 @@ app.get("/blog/:id", async (req, res) => {
 
 app.delete("/blog/:id", async (req, res) => {
   const id = req.params.id;
-  await Blog.findByIdAndDelete(id);
+  const blog = await Blog.findByIdAndDelete(id);
+  const imageName = blog.image;
+  fs.unlink(`storage/${imageName}`, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("File deleted successfully");
+    }
+  });
   res.status(200).json({
-    message:"Blog Deleted Successfully"
-  })
+    message: "Blog Deleted Successfully",
+  });
+});
+
+app.patch("/blog/:id", upload.single("image"), async (req, res) => {
+  const id = req.params.id;
+  const { title, subtitle, description } = req.body;
+  let imageName;
+  if (req.file) {
+    imageName = req.file.filename;
+    const blog = await Blog.findById(id);
+    const oldImageName = blog.image;
+    fs.unlink(`storage/${oldImageName}`, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("File deleted successfully");
+      }
+    });
+  }
+  await Blog.findByIdAndUpdate(id, {
+    title: title,
+    subtitle: subtitle,
+    description: description,
+    image:imageName
+  });
+  res.status(200).json({
+    message: "Blog Updated successfully",
+  });
 });
 
 app.use(express.static("./storage"));
